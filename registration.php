8cd263d6dbfd4992b6b36b6c23d703141fc55497
Afwan Rezal <125784343+Afwan-21b6027@users.noreply.github.com>
2023-04-27 00:19:26 +0800

    Even more updates

--- registration.php
+++ registration.php
@@ -1,28 +1,2 @@
 <?php
-    require('db.php');
-
-    $tableExists = false;
-    $result = mysqli_query($conn, "SHOW TABLES LIKE '$tableName'");
-    if (mysqli_num_rows($result) == 1) {
-        $tableExists = true;
-    }
-
-    if (!$tableExists) {
-        $sql = "CREATE TABLE $tableName (
-            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(30) NOT NULL,
-            email VARCHAR(50) NOT NULL,
-            password VARCHAR(255) NOT NULL
-        )";
-
-        if (mysqli_query($conn, $sql)) {
-            echo "Table created successfully";
-        } else {
-            echo "Error creating table: " . mysqli_error($conn);
-        }
-    }
-
-    
-
-    mysqli_close($conn);
 ?>
