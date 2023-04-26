93c39995d39764d7f3a7b4c67dc2b56afa162731
Afwan Rezal <125784343+Afwan-21b6027@users.noreply.github.com>
2023-04-27 00:29:17 +0800

    Added PHP function

--- registration.php
+++ registration.php
@@ -1,2 +1,17 @@
 <?php
+    require('db.php');
+
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    $sql_insert = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+    if (mysqli_query($conn, $sql_insert)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
 ?>
