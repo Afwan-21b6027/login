8f7960101e771efa42711888ce9c827350b68ddd
Afwan Rezal <125784343+Afwan-21b6027@users.noreply.github.com>
2023-04-27 19:22:02 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,23 @@
 <?php
-    // TODO: Create SELECT Statement to log in, should enter welcome.php
+    include('db.php');
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql_search = "SELECT * FROM users WHERE username='$username'";
+    $result = mysqli_query($conn, $sql_search);
+
+    $row = mysqli_fetch_assoc($result);
+
+
+    if (password_verify($password, $row['password'])){
+        echo "<script> 
+            alert('User Found!'); 
+            </script>";
+    } else {
+        echo "<script> 
+            alert('Account is not found!'); 
+            </script>";
+        header("Location: index.html");
+    }
 ?>
