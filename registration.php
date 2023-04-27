ce5031f7aebef400ed6ec245d34a7487ea4f5fb8
Afwan Rezal <125784343+Afwan-21b6027@users.noreply.github.com>
2023-04-27 18:47:20 +0800

    Encryption function added

--- registration.php
+++ registration.php
@@ -4,9 +4,10 @@
     $username = $_POST["username"];
     $email = $_POST["email"];
     $password = $_POST["password"];
+    $encrypted_pw = password_hash($password, PASSWORD_DEFAULT);
 
     // ! Validation of input have been stored inside valid-regis.js
-    $sql_insert = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    $sql_insert = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$encrypted_pw')";
 
     if (mysqli_query($conn, $sql_insert)) {
         echo "<script> 
