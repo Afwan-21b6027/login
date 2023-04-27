addf1449155943a39fcc0ea9de2bf8ee9de3d7b3
Afwan Rezal <125784343+Afwan-21b6027@users.noreply.github.com>
2023-04-27 10:54:25 +0800

    Update captcha.js

--- captcha.js
+++ captcha.js
@@ -11,6 +11,86 @@ function PopUpCaptcha(){
     }
 }
 
+var captcha_canvas = document.querySelector("#captcha-canvas");
+var captcha_text_element = captcha_canvas.getContext('2d');
+captcha_text_element.font = "30px Roboto";
+captcha_text_element.fillStyle = "#ffffff";
+
+var captchaInputField = document.getElementById("captcha-text-field");
+var refreshCaptchaBtn = document.getElementById("refresh-captcha-btn");
+var submitCaptchaBtn = document.getElementById("submit-captcha-btn");
+
+let alphanumeric_arr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+];
+let captchaSelection_arr =[];
+
+for (let i = 0; i < 7; i++){
+    captchaSelection_arr.push(alphanumeric_arr[Math.floor(Math.random() * alphanumeric_arr.length)]);
+}
+
+
 var exitCaptchaBtn = document.getElementById("exit-captcha-btn");
 exitCaptchaBtn.addEventListener('click', ()=>{
     document.getElementById("captcha-popup").classList.remove("popup-active");
